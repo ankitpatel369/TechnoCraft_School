c5029084054034fb7ea1535c360fa005e89d00e2
Ankit Patel <cgpatel@live.com>
2016-11-07 22:22:10 +0530

    AdmissionModels

--- TechnoCraft_School/TechnoCraftSchool_Model/Admissions/AdmissionViewModel.cs
+++ TechnoCraft_School/TechnoCraftSchool_Model/Admissions/AdmissionViewModel.cs
@@ -1,15 +1,77 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace TechnoCraftSchool_Model
 {
-    public class Admission_Additional_Info 
+    public class AdmissionViewModel
     {
-        [Key]
-        public long Admission_Additional_Info_ID { get; set; }
-
         public long Admission_ID { get; set; }
+
+        [Required]
+        [Display(Name = "Academic Year")]
+        public int AcademicYear_ID { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Middle Name")]
+        public string MiddleName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Gender")]
+        public string Gender { get; set; }
+
+        [Required]
+        [Display(Name = "Blood Group")]
+        public string BloodGroup { get; set; }
+
+        [Required]
+        [DataType(DataType.Upload)]
+        [Display(Name = "Student Photo")]
+        public string StudentPhoto { get; set; }
+
+        [Required]
+        [Display(Name = "Caste")]
+        public string CasteName { get; set; }
+
+        [Required]
+        [Display(Name = "Caste Category")]
+        public string CasteCategory { get; set; }
+
+        [Display(Name = "Course Name")]
+        public int Course_ID { get; set; }
+
+        [Display(Name = "Standard Name")]
+        public int Standard_ID { get; set; }
+
+        [Display(Name = "Class Name")]
+        public int Class_ID { get; set; }
+
+        [Display(Name = "Devision Name")]
+        public int Devision_ID { get; set; }
+
+        [Display(Name = "Date Of Admission")]
+        [DataType(DataType.DateTime)]
+        public DateTime Date_Of_Admission { get; set; }
+
+        [Display(Name = "Year Of Admission")]
+        public string Year_Of_Admission { get; set; }
+
+        [Display(Name = "Verification Date")]
+        [DataType(DataType.DateTime)]
+        public DateTime VerificationDate { get; set; }
+
+        public long Admission_Additional_Info_ID { get; set; }
 
         [Required]
         [Display(Name = "Birth Date")]
@@ -85,16 +147,8 @@ namespace TechnoCraftSchool_Model
         [EmailAddress]
         public string EmailAddress { get; set; }
 
-        public virtual Admissions Admission { get; set; }
-
-    }
-
-    public class Guardian_Details
-    {
-        [Key]
         public long Guardian_Id { get; set; }
 
-        public long Admission_ID { get; set; }
 
         [Display(Name = "Relationship With")]
         public int Relationship_ID { get; set; }
@@ -138,15 +192,7 @@ namespace TechnoCraftSchool_Model
         [Display(Name = "Annual Income")]
         public long Income { get; set; }
 
-        public virtual Admissions Admissions { get; set; }
-    }
-
-    public class Last_Attended_Details
-    {
-        [Key]
         public long Last_Attended_Details_ID { get; set; }
-
-        public long Admission_ID { get; set; }
 
         [Required]
         [Display(Name = "Name Of School")]
@@ -176,8 +222,5 @@ namespace TechnoCraftSchool_Model
         [Display(Name = "Reason for change")]
         [DataType(DataType.MultilineText)]
         public string Change_Reason { get; set; }
-
-        public virtual Admissions Admissionsl { get; set; }
-
     }
 }
--- TechnoCraft_School/TechnoCraftSchool_Model/Admissions/Admissions.cs
+++ TechnoCraft_School/TechnoCraftSchool_Model/Admissions/Admissions.cs
@@ -7,7 +7,7 @@ namespace TechnoCraftSchool_Model
     public class Admissions
     {
         [Key]
-        public int Admission_ID { get; set; }
+        public long Admission_ID { get; set; }
 
         [Required]
         [Display(Name = "Academic Year")]
@@ -46,39 +46,33 @@ namespace TechnoCraftSchool_Model
         [Display(Name = "Caste Category")]
         public string CasteCategory { get; set; }
 
-
-        [Required]
         [Display(Name = "Course Name")]
         public int Course_ID { get; set; }
 
-        [Required]
         [Display(Name = "Standard Name")]
         public int Standard_ID { get; set; }
 
-        [Required]
         [Display(Name = "Class Name")]
         public int Class_ID { get; set; }
 
-        [Required]
         [Display(Name = "Devision Name")]
         public int Devision_ID { get; set; }
 
-
-        [Required]
         [Display(Name = "Date Of Admission")]
         [DataType(DataType.DateTime)]
         public DateTime Date_Of_Admission { get; set; }
 
-        [Required]
         [Display(Name = "Year Of Admission")]
         public string Year_Of_Admission { get; set; }
 
-
-        [Required]
         [Display(Name = "Verification Date")]
         [DataType(DataType.DateTime)]
         public DateTime VerificationDate { get; set; }
 
-        public virtual ICollection<Admission_Additional_Info> Admission_Additional_Infos { get; set; }
+        public virtual Admission_Additional_Info Admission_Additional_Infos { get; set; }
+
+        public virtual Guardian_Details Guardian_Details { get; set; }
+
+        public virtual Last_Attended_Details Last_Attended_Details { get; set; }
     }
 }
